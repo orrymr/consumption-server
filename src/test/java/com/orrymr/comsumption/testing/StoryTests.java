4e05629365572bd5e10319dba147d0237be9cc10
orrym <orrym@vatit.com>
2018-06-12 18:15:24 +0200

    Start checking the Paging...

--- src/test/java/com/orrymr/comsumption/testing/StoryTests.java
+++ src/test/java/com/orrymr/comsumption/testing/StoryTests.java
@@ -6,6 +6,9 @@ import com.orrymr.consumption.repositories.StoryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -33,6 +36,21 @@ public class StoryTests {
 		List<Story> stories = (List<Story>) storyRepository.findAll();
 
 		assertThat(stories.size()).isEqualTo(1);
+	}
+
+	@Test
+	public void testPage() {
+		for (int i = 0; i < 10; i++) {
+			Story story = new Story("Orry " + i, "I this and that", 2, 4, LocalDateTime.now());
+			storyRepository.save(story);
+		}
+
+		List<Story> stories = (List<Story>) storyRepository.findAll();
+		assertThat(stories.size()).isEqualTo(10);
+
+		Page<Story> page = storyRepository.findAll(PageRequest.of(1, 5));
+
+		System.out.println(page);
 	}
 
 }
